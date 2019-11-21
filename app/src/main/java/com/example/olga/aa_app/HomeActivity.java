1ab70760c0298dd0b297a7bda06274cfbc28ec8b
Ruslan Novikov <ruslan.novikov@hotmail.de>
2019-11-21 15:32:36 +0100

    changed to androidx

--- app/src/main/java/com/example/olga/aa_app/HomeActivity.java
+++ app/src/main/java/com/example/olga/aa_app/HomeActivity.java
@@ -1,12 +1,12 @@
 package com.example.olga.aa_app;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
--- app/src/main/java/com/example/olga/aa_app/ProfileFragment.java
+++ app/src/main/java/com/example/olga/aa_app/ProfileFragment.java
@@ -2,10 +2,11 @@ package com.example.olga.aa_app;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 
 /**
