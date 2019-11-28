616cf3b599457fc43faec04b3949276afdd49de4
Lotte <lotteunckell@gmail.com>
2019-11-28 14:24:51 +0100

    basic testcases for all algorithms, closes #6

---
+++ app/src/main/java/com/example/olga/aa_app/ReactionAlgorithm.java
@@ -0,0 +1,30 @@
+package com.example.olga.aa_app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReactionAlgorithm {
+    private ArrayList<String> symptoms = new ArrayList<>();
+
+    public ArrayList<String> getSymptoms() {
+    return symptoms;
+    }
+
+    public void addSymptom(String symptom) {
+        if (!symptoms.contains(symptom)) symptoms.add(symptom);
+    }
+
+    public void addSymptoms(String[] strings) {
+        for (String symptom : strings) {
+            addSymptom(symptom);
+        }
+    }
+
+    public void removeSymptom(String symptom) {
+        symptoms.remove(symptom);
+    }
+
+    public String evaluateAlgorithm() {
+        return "";
+    }
+}
