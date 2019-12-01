6f80f7068c4f6686fba092c089171c88d540199a
Olga <omshil0305@gmail.com>
2019-12-01 19:36:38 +0100

    ScinScreen_empty

--- app/src/main/java/com/example/olga/aa_app/ScinActivity.java
+++ app/src/main/java/com/example/olga/aa_app/ScinActivity.java
@@ -1,49 +1,33 @@
 package com.example.olga.aa_app;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-
-public class SymptomeActivity extends AppCompatActivity {
-
-    Button button1;
-
+public class ScinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_symptome);
+        setContentView(R.layout.activity_scin);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.drawable.logo1);
-
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        button1 = (Button) findViewById(R.id.scin);
-
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(SymptomeActivity.this, ScinActivity.class));
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_extra, menu);
-
         return true;
     }
-
 
     public boolean onOptionsItemSelected(MenuItem item){
         if(getFragmentManager().getBackStackEntryCount() == 0) {
