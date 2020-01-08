8337f9beba2a5d78ef856047562d38ab6e40683c
Olga <omshil0305@gmail.com>
2020-01-08 20:31:15 +0100

    GMaps first implementation

--- app/src/main/java/com/example/olga/aa_app/EmergencyCallFragment.java
+++ app/src/main/java/com/example/olga/aa_app/EmergencyCallFragment.java
@@ -7,12 +7,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EmergencyCallFragment extends Fragment {
 
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     public EmergencyCallFragment() {
         // Required empty public constructor
@@ -22,8 +39,47 @@ public class EmergencyCallFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency_call, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_emergency_call, container, false);
+
+         SupportMapFragment mSupportMapFragment;
+
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_view);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map_view, mSupportMapFragment).commit();
+        }
+
+        if (mSupportMapFragment != null)
+        {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override public void onMapReady(GoogleMap googleMap) {
+                    if (googleMap != null) {
+
+                        double latitude = 17.385044;
+                        double longitude = 78.486671;
+
+                        googleMap.getUiSettings().setAllGesturesEnabled(true);
+                        MarkerOptions marker = new MarkerOptions().position(
+                                new LatLng(latitude, longitude)).title("Hello Maps");
+
+                        googleMap.addMarker(marker); // MAKE THIS WHATEVER YOU WANT
+
+                        //CameraPosition cameraPosition = new CameraPosition.Builder().target(latitude).zoom(15.0f).build();
+                        //CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                        //googleMap.moveCamera(cameraUpdate);
+
+                    }
+
+                }
+            });
+
+        // Perform any camera updates here
+
+    }
+        return v;
     }
 
 }
