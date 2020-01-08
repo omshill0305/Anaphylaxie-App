5855de318ad475c19c2b91735b86d8532e22f7c4
Olga <omshil0305@gmail.com>
2020-01-08 23:57:00 +0100

    Logo bug fixing

--- app/src/androidTest/java/com/example/olga/aa_app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/olga/aa_app/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented logo, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under logo.
        // Context appContext = InstrumentationRegistry.getTargetContext();
 
        // assertEquals("com.example.olga.aa_app", appContext.getPackageName());
