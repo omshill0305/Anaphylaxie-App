065f7c2ff26e1e494d43b16ef222a37290b70bb1
JoJo <jospa14@web.de>
2020-01-08 21:02:11 +0100

    Infos fuer Reaktionen

--- app/src/main/java/com/example/olga/aa_app/GastroIntestinalActivity.java
+++ app/src/main/java/com/example/olga/aa_app/GastroIntestinalActivity.java
@@ -1,20 +1,31 @@
 package com.example.olga.aa_app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.HashMap;
 
 public class GastroIntestinalActivity extends AppCompatActivity {
 
     Button choose;
+    HashMap<String, String> des;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +48,45 @@ public class GastroIntestinalActivity extends AppCompatActivity {
                 startActivity(new Intent(GastroIntestinalActivity.this, TreatmentRedActivity.class));
             }
         });
+
+        des = new HashMap();
+        des.put(getString(R.string.diarrhea), getString(R.string.info_diarrhea));
+        des.put(getString(R.string.abdominal_pain), getString(R.string.info_abdominal_pain));
+        des.put(getString(R.string.nausea), getString(R.string.info_nausea));
+        des.put(getString(R.string.tingling_mouth_throat), getString(R.string.info_tingling));
+        des.put(getString(R.string.vomiting), getString(R.string.info_vormiting));
+
+        setInfoButtons(R.id.listing_layout);
+    }
+
+    private void setInfoButtons(int id){
+        ViewGroup root = findViewById(id);
+        for (int i = 0; i< root.getChildCount(); i++){
+            final ViewGroup child = (ViewGroup) root.getChildAt(i);
+            ImageButton button = (ImageButton) child.getChildAt(1);
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(GastroIntestinalActivity.this, R.style.MyAlertDialogStyleInfo);
+                    CheckBox checkbox = (CheckBox) child.getChildAt(0);
+                    builder.setTitle(checkbox.getText());
+
+                    LinearLayout linear_layout = (LinearLayout) getLayoutInflater().inflate(R.layout.custom_layout_image, null);
+                    TextView text_view = (TextView) linear_layout.getChildAt(0);
+                    text_view.setText(des.get(checkbox.getText()));
+                    builder.setView(linear_layout);
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            });
+        }
     }
 
     @Override
@@ -57,4 +107,5 @@ public class GastroIntestinalActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
