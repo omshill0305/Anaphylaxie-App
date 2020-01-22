ebdcd3ea3d54bd0753a79775e89baefcfd753612
Olga <omshil0305@gmail.com>
2020-01-22 20:55:54 +0100

    Googlemap bug fix first load

--- app/src/main/java/com/example/olga/aa_app/MainActivity.java
+++ app/src/main/java/com/example/olga/aa_app/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 import android.view.Window;
 
 public class MainActivity extends Activity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
