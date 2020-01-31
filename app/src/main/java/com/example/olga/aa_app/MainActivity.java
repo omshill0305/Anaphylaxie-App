cdb9ac9d0d5d30d79f4dac1684b2b7999e709dbf
Little Bird <31522351+hummingly@users.noreply.github.com>
2020-01-31 01:54:17 +0100

    Cleanup layouts

--- app/src/main/java/com/example/olga/aa_app/MainActivity.java
+++ app/src/main/java/com/example/olga/aa_app/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/example/olga/aa_app/SplashScreen.java
+++ app/src/main/java/com/example/olga/aa_app/SplashScreen.java
@@ -13,7 +13,7 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
